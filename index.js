8fa89da1a8096cd94c1dbc4881499a454f43d9d3
Daniel Mutuku <danielmakau507@gmail.com>
2023-09-14 14:26:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+//no longer has DOM node 'main#main'
+const body = document.getElementsByTagName("body")[0]
+const main = body.querySelector("main#main")
+body.removeChild(main)
+//has a 'newHeader' variable that points to an <h1> node
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'DANIEL MAKAU MUTUKU is the champion'
